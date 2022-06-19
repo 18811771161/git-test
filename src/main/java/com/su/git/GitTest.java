1bffdc6af9256e2173f1023c25e8dbcc034c2b34
zsh <18811771161@163.com>
2022-06-19 15:28:18 +0800

    hot-fix second commit

--- src/main/java/com/su/git/GitTest.java
+++ src/main/java/com/su/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot-fix");
+        System.out.println(555);
+        System.out.println(666);
     }
 }
