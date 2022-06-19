7996bd23ec2b18a835db4f66075ba590d9abdd3b
zsh <18811771161@163.com>
2022-06-19 12:00:38 +0800

    second commit

--- src/main/java/com/su/git/GitTest.java
+++ src/main/java/com/su/git/GitTest.java
@@ -3,5 +3,7 @@ package com.su.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("first modify");
     }
 }
